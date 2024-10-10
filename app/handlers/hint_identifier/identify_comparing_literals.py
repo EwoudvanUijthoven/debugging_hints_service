235cbc8ac9670d3f3a80fc4d1064648b54e08716
Ewoud <e.k.v.uijthoven@student.tue.nl>
2024-10-10 08:20:04 +0200

    feat(MSC-45): Adding more errors

---
+++ app/handlers/hint_identifier/identify_comparing_literals.py
@@ -0,0 +1,6 @@
+"""This module contains functions to identify comparing literals."""
+
+
+def check_comparing_literals_error(code: str) -> bool:
+    """Check if there is an error related to comparing literals."""
+    pass
--- app/routers/api_endpoints.py
+++ app/routers/api_endpoints.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter
 from app.schemas.hint_request import HintRequest
-from app.handlers.identify_error import identify_error_handler
+from app.handlers.hint_identifier.identify_error import identify_error_handler
 from app.handlers.hint_generator.hint_generator_factory import hint_generator_factory
 
 router = APIRouter()
